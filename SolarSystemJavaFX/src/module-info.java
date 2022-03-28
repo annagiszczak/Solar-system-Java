503c1883f862996376159f92daeb8b23ee158f9e
wojtek5739g <wojtek5739@gmail.com>
2022-03-28 17:31:32 +0200

    List and label

--- SolarSystemJavaFX/src/module-info.java
+++ SolarSystemJavaFX/src/module-info.java
@@ -2,7 +2,8 @@ module SolarSystemJavaFX {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	requires javafx.media; //added
+	requires javafx.media;
+	requires javafx.base; //added
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens pw.edu.fizyka.pojava to javafx.graphics, javafx.fxml;
--- SolarSystemJavaFX/src/pw/edu/fizyka/pojava/MenuPane.java
+++ SolarSystemJavaFX/src/pw/edu/fizyka/pojava/MenuPane.java
@@ -1,38 +1,47 @@
 package pw.edu.fizyka.pojava;
 
+import javax.security.auth.callback.Callback;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 public class MenuPane extends BorderPane {
 
 	public MenuPane() {
-		ObservableList<String> names = FXCollections.observableArrayList("Jerry", "Elaine", "George", "Kramer",
-				"Newman", "Morty", "Helen", "Frank", "Estelle");
+		ListView<String> list = new ListView<String>();
+		ObservableList<String> items =FXCollections.observableArrayList (
+		    "S³oñce", "Merkury", "Wenus", "Ziemia", "Mars", "Jowisz", "Saturn", "Uran", "Neptun");
+		list.setItems(items);
 		
-		ListView<String> listView = new ListView<>(names);
-		listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				System.out.println("Selected item: " + newValue);
-			}
-		});
-		this.setLeft(listView); // taki 
+		
+		list.setPrefHeight(70);
+		
+		Label label1 = new Label("Info");
+		label1.setFont(new Font("Arial", 24));
+		label1.setMaxWidth(Double.MAX_VALUE);
+        label1.setAlignment(Pos.CENTER);
+		
+		VBox VBox1 = new VBox(label1, list);
+		VBox1.setStyle("-fx-background-color: #e7cbfb;");
+		
+		this.setLeft(VBox1);
 		Slider slider = new Slider(0, 100, 0);
 
         VBox vBox = new VBox(slider);
-        this.setRight(vBox); //jakies pojedyncze wstawilam ale to usun i zamien na lepsiejsze zeby to mialo rece i nogi 
-        //jak cos to pisz i pytaj  ustaw tez t³o paneli jakieœ ³adne 
-        //more on http://tutorials.jenkov.com/javafx/slider.html
-        //mozesz np dodac te elementy do tego:
-        //http://tutorials.jenkov.com/javafx/region.html
-        //zeby moc ustawic kolor
+        this.setRight(vBox); 
 	}
 
 	public MenuPane(Node arg0) {
